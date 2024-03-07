ef99c8e114a5405e8a2c2eb8018f1ea2dbec7f7e
NischaySoni <147941669+NischaySoni@users.noreply.github.com>
2024-03-07 22:35:56 +0530

    Add files via upload

---
+++ S_encoding.py
@@ -0,0 +1,15 @@
+S_opcode = {
+    "lw":"0000011",
+    "addi":"0010011", 
+    "sltiu":"0010011", 
+    "jalr":"1100111"
+    }
+
+S_operations = ["lw", "addi", "sltiu", "jalr"]
+
+S_funct3= {
+    "lw":"010", 
+    "addi":"000", 
+    "sltiu":"011", 
+    "jalr":"000"
+    }
