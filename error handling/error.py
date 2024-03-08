51c7e84cd53133d1c98fb1ddfffdb1c3104a46e1
NischaySoni <147941669+NischaySoni@users.noreply.github.com>
2024-03-08 10:27:32 +0530

    Updating error.py for all encoding

--- error handling/error.py
+++ error handling/error.py
@@ -2,14 +2,29 @@ def check_registers(instruction):
     # Splitting the instruction by commas
     registers = [reg.strip() for reg in instruction.split(',')]
     # Check if there are exactly three registers (for R Type)
+    # 
+    #
     if len(registers) != 3:
-        return False
+      return False
     else:
-        return True
-
+      return True
+    # for I type, S Type, B Type
+    #
+    #
+    if len(registers) != 2:
+      return False
+    else:
+      return True
+    # for U Type, J Type
+    #
+    #
+    if len(registers) != 1:
+      return False
+    else:
+      return True
 # Testing the function
 instruction = input("Enter the instruction: ")
 if check_registers(instruction):
-    print("All registers are present.")
+  print("All registers are present.")
 else:
-    print("Error")
+  print("Error")
