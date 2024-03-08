c97995aaa7f6808f2ccdb5c75059bf3cea6f83d6
NischaySoni <147941669+NischaySoni@users.noreply.github.com>
2024-03-08 11:20:43 +0530

    Updating error.py

--- error handling/error.py
+++ error handling/error.py
@@ -4,32 +4,30 @@ import Encoding.S_encoding as S_encoding
 import Encoding.B_encoding as B_encoding
 import Encoding.U_encoding as U_encoding
 import Encoding.J_encoding as J_encoding
-def check_instruction(instruction, valid_operations):
-    # Splitting the instruction by commas
-    registers = [reg.strip() for reg in instruction.split(',')]
+def check_instrction(instruction, valid_operations):
+  # checking for operations
+    if valid_operations not in R_operations or I_operations or S_operations or B_operations or U_operations or J_operations:
+      return False
+    else:
+      return True
     # Check if there are exactly three registers (for R Type)
     # 
     #
-    if len(registers) != 3:
+    if len(instruction) != 3:
       return False
     else:
       return True
     # for I type, S Type, B Type
     #
     #
-    if len(registers) != 2: 
+    if len(instruction) != 2: 
       return False
     else:
       return True
     # for U Type, J Type
     #
     #
-    if len(registers) != 1:
-      return False
-    else:
-      return True
-    # checking for operations
-    if operation not in R_operations or I_operations or S_operations or B_operations or U_operations or J_operations:
+    if len(instruction) != 1:
       return False
     else:
       return True
@@ -42,7 +40,7 @@ input_string = registers[0]
 register = input_string.split(",")
 # Output each register name separated by comma and space
 output_string = ", ".join(register)
-if check_instruction(output_string):
+if check_instrction(output_string, operation):
   print("Valid Instruction.")
 else:
   print("Error")
