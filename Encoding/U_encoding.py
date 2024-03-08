a7c81d132c46028504240fd038d66a6a209509e5
pranavkadam27 <pranav23382@iiitd.ac.in>
2024-03-08 15:59:00 +0530

    Updating S_encoding and U_encoding

--- Encoding/U_encoding.py
+++ Encoding/U_encoding.py
@@ -4,3 +4,17 @@ U_oppcode={
     "lui":"0110111",
     "auipc":"0010111"
 }
+
+
+def binary(n):
+    n = int(n)
+    if(n>=0):
+        bin_string = bin(abs(n))
+        bin_string = bin_string[2:]
+        bin_string = '0'*(32-len(bin_string)) + bin_string
+
+    elif(n<0):
+        bin_string = bin((abs(n)^((2**32)-1)) + 1)
+        bin_string = bin_string[2:]
+        bin_string = '1'*(32-len(bin_string)) + bin_string
+    return bin_string
