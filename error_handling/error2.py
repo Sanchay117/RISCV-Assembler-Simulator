e2a44958cb8e70195137e17d782df75dd0c62b7a
NischaySoni <147941669+NischaySoni@users.noreply.github.com>
2024-03-08 23:32:53 +0530

    added some comments

--- error_handling/error2.py
+++ error_handling/error2.py
@@ -8,10 +8,12 @@ from ..Encoding import J_encoding
 def Check_valid_immediate(value, operation):
   # For I, S, B Type
   if operation in I_encoding.I_operations or operation in S_encoding.S_operations or operation in B_encoding.B_operations:
+    # 12 bits immediate value ( -2^11 to 2^11 -1)
     if -2048 <= value <= 2047:
       return True
   # For U, J Type
   if operation in U_encoding.U_operations or operation in J_encoding.J_operations:
+    # 20 bits immediate value ( -2^19 to 2^19 -1)
     if -524288 <= value <= 524287:
       return True
   return False
