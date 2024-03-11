1e829233645e03c22e5ce4da8bd43971923047cc
pranavkadam27 <pranav23382@iiitd.ac.in>
2024-03-11 15:05:52 +0530

    made some changes in error2.py

--- error_handling/error2.py
+++ error_handling/error2.py
@@ -6,16 +6,29 @@ from ..Encoding import U_encoding
 from ..Encoding import J_encoding
 
 def Check_valid_immediate(value, operation):
-  # For I, S, B Type
-  if operation in I_encoding.I_operations or operation in S_encoding.S_operations or operation in B_encoding.B_operations:
+  # For I, S Type
+  if operation in I_encoding.I_operations or operation in S_encoding.S_operations:
     # 12 bits immediate value ( -2^11 to 2^11 -1)
     if -2048 <= value <= 2047:
       return True
-  # For U, J Type
-  if operation in U_encoding.U_operations or operation in J_encoding.J_operations:
-    # 20 bits immediate value ( -2^19 to 2^19 -1)
-    if -524288 <= value <= 524287:
+  # B type
+  if operation in B_encoding.B_operations:
+    # 13 bits immediate value (-2^12 to 2^12-1)
+    if -4096<= value <= 4095:
       return True
+  
+  # U type
+  if operation in U_encoding.U_operations:
+    # 32 bits immediate value( -2^31 to 2^31 - 1)
+    if -2147483648 <= value <= 2147483647:  #range = [-2^31, 2^31 -1]
+      return True
+    
+  # J Type
+  if operation in J_encoding.J_operations:
+    # 21 bits immediate value ( -2^20 to 2^20 -1)
+    if -1048576 <= value <= 1048575:
+      return True
+
   return False
 #
 # we need to convert label to immediate
