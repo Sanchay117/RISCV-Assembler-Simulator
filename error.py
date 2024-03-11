a7a41863edadd7c31817997257ce38db7cf18914
Sanchay Singh <sanchay072@gmail.com>
2024-03-11 19:37:08 +0530

    123 testing

--- error.py
+++ error.py
@@ -4,46 +4,12 @@ from Encoding import S_encoding
 from Encoding import B_encoding
 from Encoding import U_encoding
 from Encoding import J_encoding
+from registers import register_address
 
 # Now To Access Operations you can use R_encoding.R_operations
 # what you have done is still ok but reduces the modularity of the code
 # -Sanchay
 
-register_address = {
-    "zero": "00000",
-    "ra":"00001", 
-    "sp": "00010",
-    "gp": "00011",
-    "tp":"00100",
-    "t0":"00101",
-    "t1":"00110",
-    "t2":"00111",
-    "s0":"01000",
-    "fp":"01000",
-    "s1":"01001",
-    "a0":"01010",
-    "a1":"01011",
-    "a2":"01100",
-    "a3":"01101",
-    "a4":"01110",
-    "a5":"01111",
-    "a6":"10000",
-    "a7":"10001",
-    "s2":"10010",
-    "s3":"10011",
-    "s4":"10100",
-    "s5":"10101",
-    "s6":"10110",
-    "s7":"10111",
-    "s8":"11000",
-    "s9":"11001",
-    "s10":"11010",
-    "s11":"11011",
-    "t3":"11100",
-    "t4":"11101",
-    "t5":"11110",
-    "t6":"11111"
-}
 
 def check_instruction(operation, operands):
     # Check if the operation is in the appropriate list
