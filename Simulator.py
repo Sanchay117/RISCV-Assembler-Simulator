8990c2e75e1cb38a84d652cded8ba689a2db0efa
pranavkadam27 <pranav23382@iiitd.ac.in>
2024-04-16 19:58:06 +0530

    output file added

--- Simulator.py
+++ Simulator.py
@@ -126,12 +126,16 @@ inp=open(input_file,'r')
 lines=inp.readlines()
 inp.close()
 
+out =  []
+line_out = []
+
 pc = 0 # programm counter
 
 while pc < len(lines)*4:
     line=lines[int(pc/4)]
     oppcode=line[-7:]
 
+    
     if oppcode == R_encoding.R_oppcode:
         # to be done by sanchay
 
@@ -236,35 +240,35 @@ while pc < len(lines)*4:
 
     
 
-    if oppcode == B_encoding.B_oppcode:
-        # to be done by sanchay
-        imm=line[0] + line[-8] + line[-31:-25] + line[-12:-8] + "0"
-        funct3=line[-15:-12]
-        rs2=line[-25:-20]
-        rs1=line[-20:-15]
-        rs2_val=two_comp_to_base_10(rs2)
-        rs1_val=two_comp_to_base_10(rs1)
-        rs1_unsigned=int(rs1_val,2)
-        rs2_unsigned=int(rs2_val,2)
+    # if oppcode == B_encoding.B_oppcode:
+    #     # to be done by sanchay
+    #     imm=line[0] + line[-8] + line[-31:-25] + line[-12:-8] + "0"
+    #     funct3=line[-15:-12]
+    #     rs2=line[-25:-20]
+    #     rs1=line[-20:-15]
+    #     rs2_val=two_comp_to_base_10(rs2)
+    #     rs1_val=two_comp_to_base_10(rs1)
+    #     rs1_unsigned=int(rs1_val,2)
+    #     rs2_unsigned=int(rs2_val,2)
 
-        if funct3 == B_encoding.B_funct3["beq"]:
-            if(rs1_val==rs2_val):
-                pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
-        if funct3 == B_encoding.B_funct3["bne"]:
-            if(rs1_val!=rs2_val):
-                pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
-        if funct3 == B_encoding.B_funct3["bge"]:
-            if(rs1_val>=rs2_val):
-                pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
-        if funct3 == B_encoding.B_funct3["bgeu"]:
-            if(rs1_unsigned>=rs2_unsigned):
-                pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
-        if funct3 == B_encoding.B_funct3["blt"]:
-            if(rs1_val<rs2_val):
-                pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
-        if funct3 == B_encoding.B_funct3["bltu"]:
-            if(rs1_unsigned<rs2_unsigned):
-                pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+    #     if funct3 == B_encoding.B_funct3["beq"]:
+    #         if(rs1_val==rs2_val):
+    #             pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+    #     if funct3 == B_encoding.B_funct3["bne"]:
+    #         if(rs1_val!=rs2_val):
+    #             pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+    #     if funct3 == B_encoding.B_funct3["bge"]:
+    #         if(rs1_val>=rs2_val):
+    #             pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+    #     if funct3 == B_encoding.B_funct3["bgeu"]:
+    #         if(rs1_unsigned>=rs2_unsigned):
+    #             pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+    #     if funct3 == B_encoding.B_funct3["blt"]:
+    #         if(rs1_val<rs2_val):
+    #             pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+    #     if funct3 == B_encoding.B_funct3["bltu"]:
+    #         if(rs1_unsigned<rs2_unsigned):
+    #             pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
         # what if pc increase lets say currently we at 0 but a B instruction causes it to get 8
         # so now do we straight away goto 8 or do we go to 12 because pc+=4 after every iteration?
 
@@ -307,4 +311,30 @@ while pc < len(lines)*4:
         extended_imm = binary_to_specified_len((imm_bits + '0'), 32)  # Perform sign extension with LSB=0
         pc += two_comp_to_base_10(extended_imm)
 
-    pc+=4
+    bin_pc = bin(pc+4)[2:]
+    line_out = '0b' + binary_to_specified_len(bin_pc,32)
+
+    for register in registers:
+        line_out += '0b'+registers[register].value+' '
+
+    print(line_out)
+    line_out.rstrip(' ')
+
+    out += [line_out]
+
+    pc += 4
+    
+    
+
+
+output = open(output_file,"w")
+
+for x in out:
+    if(out.index(x) == len(lines)-1):
+        output.write(x)
+    else: 
+        output.write(x + '/n')
+
+output.close()
+
+
