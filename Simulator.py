792a2ebc15acaa7f44bb1af718ff0443b838b0d3
Sanchay Singh <sanchay072@gmail.com>
2024-04-16 21:40:00 +0530

    fixed issue with B

--- Simulator.py
+++ Simulator.py
@@ -223,27 +223,33 @@ while pc < len(lines)*4:
         rs1=line[-20:-15]
         rs2_val=two_comp_to_base_10(rs2)
         rs1_val=two_comp_to_base_10(rs1)
-        rs1_unsigned=int(rs1_val,2)
-        rs2_unsigned=int(rs2_val,2)
+        rs1_unsigned=int(rs1,2)
+        rs2_unsigned=int(rs2,2)
 
         if funct3 == B_encoding.B_funct3["beq"]:
             if(rs1_val==rs2_val):
                 pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+                break
         if funct3 == B_encoding.B_funct3["bne"]:
             if(rs1_val!=rs2_val):
                 pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+                break
         if funct3 == B_encoding.B_funct3["bge"]:
             if(rs1_val>=rs2_val):
                 pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+                break
         if funct3 == B_encoding.B_funct3["bgeu"]:
             if(rs1_unsigned>=rs2_unsigned):
                 pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+                break
         if funct3 == B_encoding.B_funct3["blt"]:
             if(rs1_val<rs2_val):
                 pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+                break
         if funct3 == B_encoding.B_funct3["bltu"]:
             if(rs1_unsigned<rs2_unsigned):
                 pc+=two_comp_to_base_10(binary_to_specified_len(imm,32))
+                break
         # what if pc increase lets say currently we at 0 but a B instruction causes it to get 8
         # so now do we straight away goto 8 or do we go to 12 because pc+=4 after every iteration?
 
